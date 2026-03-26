2dc068a0a973659a6b87d508000e9d835a6085e3
David <david851215@gmail.com>
2026-03-26 10:48:07 +0800

    style: [TaskCard] revise the missing of style group

--- app/ui/components/shared/TaskCard.tsx
+++ app/ui/components/shared/TaskCard.tsx
@@ -11,7 +11,7 @@ export default function TaskCard({ data }: TaskCardProps) {
     <Link
       key={data.id}
       href={`/task/${data.id}`}
-      className="p-4 bg-white border border-slate-200 rounded-xl hover:shadow-md transition flex justify-between items-center gap-8"
+      className="p-4 bg-white border border-slate-200 rounded-xl hover:shadow-md transition flex justify-between items-center gap-8 group"
     >
       <div>
         <TaskPriorityLabel data={data.priority} />
