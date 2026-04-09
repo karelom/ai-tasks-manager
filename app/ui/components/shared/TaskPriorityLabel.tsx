f6d24ff9221243133fca7671f6d338c95e7701a0
David <david851215@gmail.com>
2026-04-09 23:35:07 +0800

    feat: [TaskPriorityLabel] add edit feature

--- app/ui/components/shared/TaskPriorityLabel.tsx
+++ app/ui/components/shared/TaskPriorityLabel.tsx
@@ -1,14 +1,53 @@
+'use client';
+
+import { updateTask } from '@/lib/actions';
 import { TaskPriority } from '@/lib/definitions';
+import { cn } from '@/lib/utils';
+import { InlineSelect, InlineSelectOption } from '@/ui/components/shared/InlineSelect';
 import { ChartBarIcon } from '@heroicons/react/24/outline';
-import clsx from 'clsx';
 
 interface TaskPriorityLabelProps {
   data: TaskPriority;
+  taskId?: string;
 }
 
-export default function TaskPriorityLabel({ data }: TaskPriorityLabelProps) {
+const PRIORITY_OPTIONS: InlineSelectOption<TaskPriority>[] = [
+  { value: TaskPriority.NONE, label: 'None' },
+  { value: TaskPriority.LOW, label: 'Low' },
+  { value: TaskPriority.MEDIUM, label: 'Medium' },
+  { value: TaskPriority.HIGH, label: 'High' },
+];
+
+export default function TaskPriorityLabel({ data, taskId }: TaskPriorityLabelProps) {
+  const handleStatusChange = async (priority: TaskPriority) => {
+    if (!taskId) return { ok: true };
+    return await updateTask(taskId, { priority });
+  };
+
+  if (taskId) {
+    return (
+      <InlineSelect<TaskPriority>
+        data={data}
+        options={PRIORITY_OPTIONS}
+        shortcut="P"
+        placeholder="Search priority ..."
+        renderTrigger={(selected) => (
+          <DisplayLabel
+            data={selected}
+            className="cursor-pointer px-3 py-1 hover:bg-slate-50 hover:shadow-md rounded-full"
+          />
+        )}
+        onSave={handleStatusChange}
+      />
+    );
+  } else {
+    return <DisplayLabel data={data} />;
+  }
+}
+
+function DisplayLabel({ data, className }: { data: TaskPriority; className?: string }) {
   return (
-    <div className={clsx('flex gap-2', taskPriorityClass(data))}>
+    <div className={cn('flex gap-2', className, taskPriorityClass(data))}>
       <ChartBarIcon className="w-4" />
       {data}
     </div>
