4d8d5c8f8b8f25a2a8c73aa826851ac37561710e
David <david851215@gmail.com>
2026-04-14 10:33:43 +0800

    fixup! style: [Sidebar] fix the main content layout from not showing extra tasks
    addtional code

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -11,12 +11,12 @@ export default function Layout({
 }) {
   return (
     <div>
-      <div className="flex-1 flex flex-col relative overflow-y-auto">
+      <div className="h-screen flex-1 flex flex-col relative">
         <Header />
 
-        <div className="flex h-screen overflow-hidden">
+        <div className="flex flex-1 overflow-hidden">
           <Sidebar />
-          <main className="flex-1 p-8 bg-slate-50/50 min-h-screen">
+          <main className="flex-1 p-8 bg-slate-50/50 overflow-y-auto">
             {children}
             {modal}
           </main>
