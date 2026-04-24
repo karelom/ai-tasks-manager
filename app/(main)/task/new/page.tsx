50b01160d8d056c562131a7ab4e83dd1a5c482d1
David <david851215@gmail.com>
2026-04-24 14:54:24 +0800

    refactor: [NewTaskPage] make it a server component

--- app/(main)/task/new/page.tsx
+++ app/(main)/task/new/page.tsx
@@ -1,22 +1,20 @@
-'use client';
-
-import CreateTaskForm from '@/ui/components/shared/CreateTaskForm';
-import { useSearchParams } from 'next/navigation';
+import CreateTaskForm, { CreateTaskProps } from '@/ui/components/shared/CreateTaskForm';
 import { Suspense } from 'react';
 
-export default function Page() {
+type PageProps = {
+  searchParams: Promise<CreateTaskProps>;
+};
+
+export default function Page({ searchParams }: PageProps) {
   return (
     <Suspense fallback={<div>loading form ...</div>}>
-      <MainContent />
+      <MainContent searchParams={searchParams} />
     </Suspense>
   );
 }
 
-function MainContent() {
-  const searchParams = useSearchParams();
-  const projectId = searchParams.get('projectId');
-  const parentId = searchParams.get('parentId');
-
+async function MainContent({ searchParams }: PageProps) {
+  const { projectId, parentId } = await searchParams;
   return (
     <>
       <h2 className="text-xl font-semibold text-slate-800 mb-8">Create New Task</h2>
